67f4f97a246725a9e6770a74fc3445dfd52d701d
Yeshwant <yeshwant@Yeshwants-MacBook-Pro.local>
2019-11-23 13:22:01 -0600

    basic client commands

--- src/client.java
+++ src/client.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
+
 public class client {
 
     public static void main(String[] args){
 
+        //call method that adds concepts into queue from dictionary
 
+    System.out.println("What do you want to do?");
+    System.out.println("Type in 'help' for options");
+
+    Scanner input = new Scanner(System.in);
 
 
 
